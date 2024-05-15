f1d0a61304389e7b2d6b31608d343fbef90d048f
drzo <d@rzo.io>
2024-05-15 17:22:01 +0200

    Initialize shipit-deploy/stable_3_55

---
+++ packages/app/src/cli/services/context/identifiers.ts
@@ -0,0 +1,71 @@
+import {deployConfirmed} from './identifiers-extensions.js'
+import {configExtensionsIdentifiersBreakdown, extensionsIdentifiersDeployBreakdown} from './breakdown-extensions.js'
+import {AppInterface} from '../../models/app/app.js'
+import {Identifiers} from '../../models/app/identifiers.js'
+import {MinimalOrganizationApp} from '../../models/organization.js'
+import {deployOrReleaseConfirmationPrompt} from '../../prompts/deploy-release.js'
+import {AbortSilentError} from '@shopify/cli-kit/node/error'
+
+export type PartnersAppForIdentifierMatching = MinimalOrganizationApp
+
+export interface EnsureDeploymentIdsPresenceOptions {
+  app: AppInterface
+  token: string
+  appId: string
+  appName: string
+  envIdentifiers: Partial<Identifiers>
+  force: boolean
+  release: boolean
+  partnersApp?: PartnersAppForIdentifierMatching
+  includeDraftExtensions?: boolean
+}
+
+export interface RemoteSource {
+  uuid: string
+  type: string
+  id: string
+  title: string
+  draftVersion?: {config: string}
+}
+
+export interface LocalSource {
+  localIdentifier: string
+  graphQLType: string
+  type: string
+  handle: string
+}
+
+export async function ensureDeploymentIdsPresence(options: EnsureDeploymentIdsPresenceOptions) {
+  const {extensionIdentifiersBreakdown, extensionsToConfirm, remoteExtensionsRegistrations} =
+    await extensionsIdentifiersDeployBreakdown(options)
+
+  const configExtensionIdentifiersBreakdown = await configExtensionsIdentifiersBreakdown({
+    token: options.token,
+    apiKey: options.appId,
+    localApp: options.app,
+    release: options.release,
+  })
+
+  const confirmed = await deployOrReleaseConfirmationPrompt({
+    extensionIdentifiersBreakdown,
+    configExtensionIdentifiersBreakdown,
+    appTitle: options.partnersApp?.title,
+    release: options.release,
+    force: options.force,
+  })
+  if (!confirmed) throw new AbortSilentError()
+
+  const result = await deployConfirmed(
+    options,
+    remoteExtensionsRegistrations.extensionRegistrations,
+    remoteExtensionsRegistrations.configurationRegistrations,
+    extensionsToConfirm,
+  )
+
+  return {
+    app: options.appId,
+    extensions: result.extensions,
+    extensionIds: result.extensionIds,
+    extensionsNonUuidManaged: result.extensionsNonUuidManaged,
+  }
+}
---
+++ packages/app/src/cli/services/generate.test.ts
@@ -0,0 +1,202 @@
+import generate from './generate.js'
+import {ensureGenerateContext} from './context.js'
+import {generateExtensionTemplate} from './generate/extension.js'
+import {fetchPartnersSession} from './context/partner-account-info.js'
+import {loadApp} from '../models/app/loader.js'
+import {
+  testAppWithConfig,
+  testFunctionExtension,
+  testLocalExtensionTemplates,
+  testRemoteSpecifications,
+  testRemoteExtensionTemplates,
+  testThemeExtensions,
+  testPartnersUserSession,
+} from '../models/app/app.test-data.js'
+import {ExtensionInstance} from '../models/extensions/extension-instance.js'
+import generateExtensionPrompts from '../prompts/generate/extension.js'
+import {describe, expect, vi, afterEach, test} from 'vitest'
+import {Config} from '@oclif/core'
+import {partnersRequest} from '@shopify/cli-kit/node/api/partners'
+import {joinPath} from '@shopify/cli-kit/node/path'
+import {mockAndCaptureOutput} from '@shopify/cli-kit/node/testing/output'
+
+vi.mock('../constants.js', async () => {
+  const actual: any = await vi.importActual('../constants.js')
+  return {
+    ...actual,
+    blocks: {
+      ...actual.blocks,
+      functions: {
+        defaultRegistrationLimit: 1,
+      },
+    },
+  }
+})
+vi.mock('../models/app/loader.js')
+vi.mock('../prompts/generate/extension.js')
+vi.mock('../services/generate/extension.js')
+vi.mock('../services/context.js')
+vi.mock('@shopify/cli-kit/node/api/partners')
+vi.mock('./local-storage.js')
+vi.mock('./context/partner-account-info.js')
+
+afterEach(() => {
+  mockAndCaptureOutput().clear()
+})
+
+describe('generate', () => {
+  const mockConfig = new Config({root: ''})
+  test('displays a confirmation message with instructions to run dev', async () => {
+    // Given
+    const outputInfo = await mockSuccessfulCommandExecution('subscription_ui')
+
+    // When
+    await generate({directory: '/', reset: false, commandConfig: mockConfig})
+
+    // Then
+    expect(outputInfo.info()).toMatchInlineSnapshot(`
+      "╭─ success ────────────────────────────────────────────────────────────────────╮
+      │                                                                              │
+      │  Your extension was created in extensions/name.                              │
+      │                                                                              │
+      │  Next steps                                                                  │
+      │    • To preview this extension along with the rest of the project, run       │
+      │      \`yarn dev\`                                                              │
+      │                                                                              │
+      ╰──────────────────────────────────────────────────────────────────────────────╯
+      "
+    `)
+  })
+
+  test('displays a confirmation message for a theme app extension', async () => {
+    // Given
+    const outputInfo = await mockSuccessfulCommandExecution('theme_app_extension')
+
+    // When
+    await generate({directory: '/', reset: false, commandConfig: mockConfig})
+
+    // Then
+    expect(outputInfo.info()).toMatchInlineSnapshot(`
+      "╭─ success ────────────────────────────────────────────────────────────────────╮
+      │                                                                              │
+      │  Your extension was created in extensions/name.                              │
+      │                                                                              │
+      │  Next steps                                                                  │
+      │    • To preview this extension along with the rest of the project, run       │
+      │      \`yarn dev\`                                                              │
+      │                                                                              │
+      ╰──────────────────────────────────────────────────────────────────────────────╯
+      "
+    `)
+  })
+
+  test('displays a confirmation message for a function', async () => {
+    // Given
+    const outputInfo = await mockSuccessfulCommandExecution('product_discounts')
+
+    // When
+    await generate({directory: '/', reset: false, commandConfig: mockConfig})
+
+    // Then
+    expect(outputInfo.info()).toMatchInlineSnapshot(`
+      "╭─ success ────────────────────────────────────────────────────────────────────╮
+      │                                                                              │
+      │  Your extension was created in extensions/name.                              │
+      │                                                                              │
+      │  Reference                                                                   │
+      │    • For more details, see the docs [1]                                      │
+      │                                                                              │
+      ╰──────────────────────────────────────────────────────────────────────────────╯
+      [1] https://shopify.dev/docs/apps/discounts
+      "
+    `)
+  })
+
+  test('throws error if trying to generate a non existing type', async () => {
+    // Given
+    await mockSuccessfulCommandExecution('unknown_type')
+
+    // When
+    const got = generate({directory: '/', reset: false, commandConfig: mockConfig, template: 'unknown_type'})
+
+    // Then
+    await expect(got).rejects.toThrow(/Unknown extension type: unknown_type/)
+  })
+
+  test('throws error if trying to generate a extension over the registration limit', async () => {
+    // Given
+    const themeExtension = await testThemeExtensions()
+    await mockSuccessfulCommandExecution('theme_app_extension', [themeExtension])
+
+    // When
+    const got = generate({directory: '/', reset: false, commandConfig: mockConfig, template: 'theme_app_extension'})
+
+    // Then
+    await expect(got).rejects.toThrow(/Invalid extension type/)
+  })
+
+  test('throws error if trying to generate a function over the registration limit', async () => {
+    // Given
+    const discountsFunction = await testFunctionExtension()
+    await mockSuccessfulCommandExecution('product_discounts', [discountsFunction])
+
+    // When
+    const got = generate({directory: '/', reset: false, commandConfig: mockConfig, template: 'product_discounts'})
+
+    // Then
+    await expect(got).rejects.toThrow(/Invalid extension type/)
+  })
+
+  test('throws error if trying to generate with an unsupported flavor', async () => {
+    // Given
+    await mockSuccessfulCommandExecution('subscription_ui')
+
+    // When
+    const got = generate({
+      directory: '/',
+      reset: false,
+      commandConfig: mockConfig,
+      template: 'subscription_ui',
+      flavor: 'unknown',
+    })
+
+    // Then
+    await expect(got).rejects.toThrow(/Invalid template for extension type/)
+  })
+})
+
+async function mockSuccessfulCommandExecution(identifier: string, existingExtensions: ExtensionInstance[] = []) {
+  vi.mocked(fetchPartnersSession).mockResolvedValue(testPartnersUserSession)
+
+  const appRoot = '/'
+  const app = testAppWithConfig({
+    app: {
+      directory: appRoot,
+      extensionsForType: (_spec: {identifier: string; externalIdentifier: string}) => existingExtensions,
+      allExtensions: existingExtensions,
+    },
+    config: {path: joinPath(appRoot, 'shopify.app.toml')},
+  })
+
+  const allExtensionTemplates = testRemoteExtensionTemplates.concat(testLocalExtensionTemplates)
+  const extensionTemplate = allExtensionTemplates.find((spec) => spec.identifier === identifier)!
+
+  vi.mocked(loadApp).mockResolvedValue(app)
+  vi.mocked(partnersRequest).mockResolvedValueOnce({extensionSpecifications: testRemoteSpecifications})
+  vi.mocked(partnersRequest).mockResolvedValueOnce({templateSpecifications: testRemoteExtensionTemplates})
+  vi.mocked(ensureGenerateContext).mockResolvedValue('api-key')
+  vi.mocked(generateExtensionPrompts).mockResolvedValue({
+    extensionTemplate,
+    extensionContent: [
+      {
+        index: 0,
+        name: identifier,
+        flavor: 'vanilla-js',
+      },
+    ],
+  })
+  vi.mocked(generateExtensionTemplate).mockResolvedValue([
+    {directory: joinPath('extensions', 'name'), extensionTemplate},
+  ])
+  return mockAndCaptureOutput()
+}
