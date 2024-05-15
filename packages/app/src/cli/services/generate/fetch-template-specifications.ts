51303c703ae839e8c60576da3423596a61094774
drzo <d@rzo.io>
2024-05-15 17:00:15 +0200

    Initialize flow-extensions-as-config-extensions

---
+++ packages/app/src/cli/services/generate/fetch-template-specifications.ts
@@ -0,0 +1,52 @@
+import {
+  RemoteTemplateSpecificationsQuery,
+  RemoteTemplateSpecificationsQuerySchema,
+} from '../../api/graphql/template_specifications.js'
+import {ExtensionTemplate} from '../../models/app/template.js'
+import themeExtension from '../../models/templates/theme-specifications/theme.js'
+import checkoutPostPurchaseExtension from '../../models/templates/ui-specifications/checkout_post_purchase.js'
+import checkoutUIExtension from '../../models/templates/ui-specifications/checkout_ui_extension.js'
+import customerAccountsUIExtension from '../../models/templates/ui-specifications/customer_accounts_ui_extension.js'
+import flowActionDefinitionExtension from '../../models/templates/ui-specifications/flow_action_definition.js'
+import flowTriggerDefinitionExtension from '../../models/templates/ui-specifications/flow_trigger_definition.js'
+import posUIExtension from '../../models/templates/ui-specifications/pos_ui_extension.js'
+import productSubscriptionUIExtension from '../../models/templates/ui-specifications/product_subscription.js'
+import taxCalculationUIExtension from '../../models/templates/ui-specifications/tax_calculation.js'
+import UIExtension from '../../models/templates/ui-specifications/ui_extension.js'
+import webPixelUIExtension from '../../models/templates/ui-specifications/web_pixel_extension.js'
+import {partnersRequest} from '@shopify/cli-kit/node/api/partners'
+
+export async function fetchExtensionTemplates(
+  token: string,
+  apiKey: string,
+  availableSpecifications: string[],
+): Promise<ExtensionTemplate[]> {
+  const remoteTemplates: RemoteTemplateSpecificationsQuerySchema = await partnersRequest(
+    RemoteTemplateSpecificationsQuery,
+    token,
+    {apiKey},
+  )
+  const localTemplates = localExtensionTemplates(availableSpecifications)
+  return remoteTemplates.templateSpecifications.concat(localTemplates)
+}
+
+export function localExtensionTemplates(availableSpecifications: string[]): ExtensionTemplate[] {
+  const allLocalTemplates = [
+    themeExtension,
+    checkoutPostPurchaseExtension,
+    checkoutUIExtension,
+    customerAccountsUIExtension,
+    posUIExtension,
+    productSubscriptionUIExtension,
+    taxCalculationUIExtension,
+    UIExtension,
+    webPixelUIExtension,
+    flowTriggerDefinitionExtension,
+    flowActionDefinitionExtension,
+  ]
+  return allLocalTemplates.filter(
+    (template) =>
+      availableSpecifications.includes(template.identifier) ||
+      availableSpecifications.includes(template.types[0]!.type),
+  )
+}
