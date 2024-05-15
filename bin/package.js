272c6a885900374eda20f3ba54a980d98e1d4667
drzo <d@rzo.io>
2024-05-15 16:46:40 +0200

    Initialize package-debian-and-rpm

---
+++ bin/package.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+import {constants, file, output, path, template} from '@shopify/cli-kit';
+import {homebrewVariables, copyHomebrew} from '../packaging/lib/homebrew.js';
+import {packageDebian} from '../packaging/lib/debian.js';
+import {packageRPM} from '../packaging/lib/rpm.js';
+
+output.initiateLogging();
+
+const cliVersion = await constants.versions.cliKit();
+const packagingDir = path.join(path.dirname(import.meta.url), '../packaging').replace(/^file:/, '')
+const distDir = path.join(packagingDir, 'dist')
+
+await file.rmdir(distDir, {force: true})
+await template.recursiveDirectoryCopy(
+  path.join(packagingDir, 'src'),
+  distDir,
+  {
+    cliVersion,
+    ...(await homebrewVariables(cliVersion)),
+  },
+);
+
+await Promise.all([
+  copyHomebrew(distDir),
+  packageDebian(distDir, cliVersion),
+  packageRPM(distDir),
+])
---
+++ packages/cli-kit/src/plugins.ts
@@ -0,0 +1,74 @@
+import {join, pathToFileURL} from './path.js'
+import {debug, content} from './output.js'
+import {JsonMap} from './json.js'
+import {PickByPrefix} from './typing/pick-by-prefix.js'
+import {MonorailEventPublic} from './monorail.js'
+import {HookReturnPerTunnelPlugin} from './plugins/tunnel.js'
+import {Interfaces} from '@oclif/core'
+
+const TUNNEL_PLUGINS = ['@shopify/plugin-ngrok']
+
+interface TunnelPlugin {
+  start: (options: TunnelStartOptions) => Promise<string>
+}
+
+interface TunnelStartOptions {
+  port: number
+}
+
+export async function lookupTunnelPlugin(plugins: Interfaces.Plugin[]): Promise<TunnelPlugin | undefined> {
+  debug(content`Looking up the Ngrok tunnel plugin...`)
+  const tunnelPlugin = plugins.find((plugin) => TUNNEL_PLUGINS.includes(plugin.name))
+  if (!tunnelPlugin) return undefined
+  const tunnelPath = pathToFileURL(join(tunnelPlugin.root, 'dist/tunnel.js')).toString()
+  return import(tunnelPath).catch(() => undefined)
+}
+
+/**
+ * Convenience function to trigger a hook, and gather any successful responses. Failures are ignored.
+ *
+ * Responses are organised into a dictionary, keyed by plug-in name. Only plug-ins that have hooks registered for the given event, and the hooks were run successfully, are included.
+ */
+export async function fanoutHooks<TPluginMap extends HookReturnsPerPlugin, TEvent extends string & keyof TPluginMap>(
+  config: Interfaces.Config,
+  event: TEvent,
+  options: TPluginMap[typeof event]['options'],
+  timeout?: number,
+): Promise<Partial<TPluginMap[typeof event]['pluginReturns']>> {
+  const res = await config.runHook(event, options, timeout)
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  return Object.fromEntries(res.successes.map(({result, plugin}) => [plugin.name, result])) as any
+}
+
+type AppSpecificMonorailFields = PickByPrefix<MonorailEventPublic, 'app_', 'project_type' | 'api_key' | 'partner_id'> &
+  PickByPrefix<MonorailEventPublic, 'cmd_extensions_'> &
+  PickByPrefix<MonorailEventPublic, 'cmd_scaffold_'>
+
+interface HookReturnsPerPlugin extends HookReturnPerTunnelPlugin {
+  public_command_metadata: {
+    options: {[key: string]: never}
+    pluginReturns: {
+      '@shopify/app': Partial<AppSpecificMonorailFields>
+      [pluginName: string]: JsonMap
+    }
+  }
+  [hookName: string]: {
+    options: {[key: string]: unknown}
+    pluginReturns: {[key: string]: JsonMap}
+  }
+}
+
+export type PluginReturnsForHook<
+  TEvent extends keyof TPluginMap,
+  TPluginName extends keyof TPluginMap[TEvent]['pluginReturns'],
+  TPluginMap extends HookReturnsPerPlugin = HookReturnsPerPlugin,
+> = TPluginMap[TEvent]['pluginReturns'][TPluginName]
+
+export type FanoutHookFunction<
+  TEvent extends keyof TPluginMap = string,
+  TPluginName extends keyof TPluginMap[TEvent]['pluginReturns'] = string,
+  TPluginMap extends HookReturnsPerPlugin = HookReturnsPerPlugin,
+> = (
+  this: Interfaces.Hook.Context,
+  options: TPluginMap[TEvent]['options'] & {config: Interfaces.Config},
+) => Promise<PluginReturnsForHook<TEvent, TPluginName, TPluginMap>>
