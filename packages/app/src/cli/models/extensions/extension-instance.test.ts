8b2c6a93f27deba2d88d13119c162625bde7c2e4
drzo <d@rzo.io>
2024-05-15 17:20:34 +0200

    Initialize thomfoolery-test-cla-action-1

---
+++ packages/app/src/cli/models/extensions/extension-instance.test.ts
@@ -0,0 +1,262 @@
+import {
+  testApp,
+  testAppConfigExtensions,
+  testFunctionExtension,
+  testTaxCalculationExtension,
+  testThemeExtensions,
+  testUIExtension,
+  testWebPixelExtension,
+  testWebhookExtensions,
+} from '../app/app.test-data.js'
+import {FunctionConfigType} from '../extensions/specifications/function.js'
+import {ExtensionBuildOptions} from '../../services/build/extension.js'
+import {joinPath} from '@shopify/cli-kit/node/path'
+import {describe, expect, test} from 'vitest'
+import {inTemporaryDirectory, readFile} from '@shopify/cli-kit/node/fs'
+import {Writable} from 'stream'
+
+function functionConfiguration(): FunctionConfigType {
+  return {
+    name: 'foo',
+    type: 'function',
+    api_version: '2023-07',
+    configuration_ui: true,
+    metafields: [],
+    build: {},
+  }
+}
+
+describe('watchPaths', async () => {
+  test('returns an array for a single path', async () => {
+    const config = functionConfiguration()
+    config.build = {
+      watch: 'src/single-path.foo',
+    }
+    const extensionInstance = await testFunctionExtension({
+      config,
+      dir: 'foo',
+    })
+
+    const got = extensionInstance.watchPaths
+
+    expect(got).toEqual([joinPath('foo', 'src', 'single-path.foo'), joinPath('foo', '**', '!(.)*.graphql')])
+  })
+
+  test('returns default paths for javascript', async () => {
+    const config = functionConfiguration()
+    config.build = {}
+    const extensionInstance = await testFunctionExtension({
+      config,
+      entryPath: 'src/index.js',
+      dir: 'foo',
+    })
+
+    const got = extensionInstance.watchPaths
+
+    expect(got).toEqual([joinPath('foo', 'src', '**', '*.{js,ts}'), joinPath('foo', '**', '!(.)*.graphql')])
+  })
+
+  test('returns js and ts paths for esbuild extensions', async () => {
+    const extensionInstance = await testUIExtension({directory: 'foo'})
+
+    const got = extensionInstance.watchPaths
+
+    expect(got).toEqual([joinPath('foo', 'src', '**', '*.{ts,tsx,js,jsx}')])
+  })
+
+  test('return empty array for non-function non-esbuild extensions', async () => {
+    const extensionInstance = await testTaxCalculationExtension('foo')
+
+    const got = extensionInstance.watchPaths
+
+    expect(got).toEqual([])
+  })
+
+  test('returns configured paths and input query', async () => {
+    const config = functionConfiguration()
+    config.build = {
+      watch: ['src/**/*.rs', 'src/**/*.foo'],
+    }
+    const extensionInstance = await testFunctionExtension({
+      config,
+      dir: 'foo',
+    })
+
+    const got = extensionInstance.watchPaths
+
+    expect(got).toEqual([
+      joinPath('foo', 'src/**/*.rs'),
+      joinPath('foo', 'src/**/*.foo'),
+      joinPath('foo', '**', '!(.)*.graphql'),
+    ])
+  })
+
+  test('returns null if not javascript and not configured', async () => {
+    const config = functionConfiguration()
+    config.build = {}
+    const extensionInstance = await testFunctionExtension({
+      config,
+    })
+
+    const got = extensionInstance.watchPaths
+
+    expect(got).toBeNull()
+  })
+})
+
+describe('isDraftable', () => {
+  test('returns false for theme extensions', async () => {
+    const extensionInstance = await testThemeExtensions()
+
+    const got1 = extensionInstance.isDraftable()
+
+    expect(got1).toBe(false)
+  })
+
+  test('returns false for app config extensions', async () => {
+    const extensionInstance = await testAppConfigExtensions()
+
+    const got1 = extensionInstance.isDraftable()
+
+    expect(got1).toBe(false)
+  })
+
+  test('returns true for web pixel extensions', async () => {
+    const extensionInstance = await testWebPixelExtension()
+
+    const got = extensionInstance.isDraftable()
+
+    expect(got).toBe(true)
+  })
+
+  test('returns true for ui extensions', async () => {
+    const extensionInstance = await testUIExtension()
+
+    const got = extensionInstance.isDraftable()
+
+    expect(got).toBe(true)
+  })
+
+  test('returns true for functions', async () => {
+    const extensionInstance = await testFunctionExtension()
+
+    const got = extensionInstance.isDraftable()
+
+    expect(got).toBe(true)
+  })
+})
+
+describe('build', async () => {
+  test('creates a valid JS file for tax calculation extensions', async () => {
+    await inTemporaryDirectory(async (tmpDir) => {
+      // Given
+      const extensionInstance = await testTaxCalculationExtension(tmpDir)
+      const options: ExtensionBuildOptions = {
+        stdout: new Writable(),
+        stderr: new Writable(),
+        app: testApp(),
+        environment: 'production',
+      }
+
+      const outputFilePath = joinPath(tmpDir, `dist/${extensionInstance.outputFileName}`)
+
+      // When
+      await extensionInstance.build(options)
+
+      // Then
+      const outputFileContent = await readFile(outputFilePath)
+      expect(outputFileContent).toEqual('(()=>{})();')
+    })
+  })
+})
+
+describe('deployConfig', async () => {
+  test('returns deployConfig when defined', async () => {
+    const extensionInstance = await testThemeExtensions()
+
+    const got = await extensionInstance.deployConfig({token: 'token', apiKey: 'apiKey'})
+
+    expect(got).toMatchObject({theme_extension: {files: {}}})
+  })
+
+  test('returns transformed config when defined', async () => {
+    const extensionInstance = await testAppConfigExtensions()
+
+    const got = await extensionInstance.deployConfig({token: 'token', apiKey: 'apiKey'})
+
+    expect(got).toMatchObject({embedded: true})
+  })
+
+  test('returns undefined when the transformed config is empty', async () => {
+    const extensionInstance = await testAppConfigExtensions(true)
+
+    const got = await extensionInstance.deployConfig({token: 'token', apiKey: 'apiKey'})
+
+    expect(got).toBeUndefined()
+  })
+})
+
+describe('bundleConfig', async () => {
+  test('returns the uuid from extensions when the extension is uuid managed', async () => {
+    const extensionInstance = await testThemeExtensions()
+
+    const got = await extensionInstance.bundleConfig({
+      identifiers: {
+        extensions: {'theme-extension-name': 'theme-uuid'},
+        extensionIds: {},
+        app: 'My app',
+        extensionsNonUuidManaged: {},
+      },
+      token: 'token',
+      apiKey: 'apiKey',
+    })
+
+    expect(got).toEqual(
+      expect.objectContaining({
+        uuid: 'theme-uuid',
+      }),
+    )
+  })
+
+  test('returns the uuid from extensionsNonUuidManaged when the extension is not uuid managed', async () => {
+    const extensionInstance = await testAppConfigExtensions()
+
+    const got = await extensionInstance.bundleConfig({
+      identifiers: {
+        extensions: {},
+        extensionIds: {},
+        app: 'My app',
+        extensionsNonUuidManaged: {'point-of-sale': 'uuid'},
+      },
+      token: 'token',
+      apiKey: 'apiKey',
+    })
+
+    expect(got).toEqual(
+      expect.objectContaining({
+        uuid: 'uuid',
+      }),
+    )
+  })
+
+  test('returns arrays formatted properly inside the config', async () => {
+    const extensionInstance = await testWebhookExtensions()
+
+    const got = await extensionInstance.bundleConfig({
+      identifiers: {
+        extensions: {},
+        extensionIds: {},
+        app: 'My app',
+        extensionsNonUuidManaged: {webhooks: 'uuid'},
+      },
+      token: 'token',
+      apiKey: 'apiKey',
+    })
+
+    expect(got).toEqual(
+      expect.objectContaining({
+        config: '{"subscriptions":[{"uri":"https://my-app.com/webhooks","topic":"orders/delete"}]}',
+      }),
+    )
+  })
+})
---
+++ packages/app/src/cli/services/context/identifiers-extensions.ts
@@ -0,0 +1,160 @@
+import {manualMatchIds} from './id-manual-matching.js'
+import {automaticMatchmaking} from './id-matching.js'
+import {EnsureDeploymentIdsPresenceOptions, LocalSource, RemoteSource} from './identifiers.js'
+import {extensionMigrationPrompt, matchConfirmationPrompt} from './prompts.js'
+import {createExtension} from '../dev/create-extension.js'
+import {IdentifiersExtensions} from '../../models/app/identifiers.js'
+import {getUIExtensionsToMigrate, migrateExtensionsToUIExtension} from '../dev/migrate-to-ui-extension.js'
+import {getFlowExtensionsToMigrate, migrateFlowExtensions} from '../dev/migrate-flow-extension.js'
+import {AppInterface} from '../../models/app/app.js'
+import {ensureAuthenticatedPartners} from '@shopify/cli-kit/node/session'
+import {outputCompleted} from '@shopify/cli-kit/node/output'
+import {AbortSilentError} from '@shopify/cli-kit/node/error'
+
+interface AppWithExtensions {
+  extensionRegistrations: RemoteSource[]
+  dashboardManagedExtensionRegistrations: RemoteSource[]
+}
+
+export async function ensureExtensionsIds(
+  options: EnsureDeploymentIdsPresenceOptions,
+  {
+    extensionRegistrations: initialRemoteExtensions,
+    dashboardManagedExtensionRegistrations: dashboardOnlyExtensions,
+  }: AppWithExtensions,
+) {
+  let remoteExtensions = initialRemoteExtensions
+  const validIdentifiers = options.envIdentifiers.extensions ?? {}
+  const localExtensions = options.app.allExtensions.filter((ext) => ext.isUuidManaged())
+
+  const uiExtensionsToMigrate = getUIExtensionsToMigrate(localExtensions, remoteExtensions, validIdentifiers)
+  const flowExtensionsToMigrate = getFlowExtensionsToMigrate(localExtensions, dashboardOnlyExtensions, validIdentifiers)
+
+  if (uiExtensionsToMigrate.length > 0) {
+    const confirmedMigration = await extensionMigrationPrompt(uiExtensionsToMigrate)
+    if (!confirmedMigration) throw new AbortSilentError()
+    remoteExtensions = await migrateExtensionsToUIExtension(uiExtensionsToMigrate, options.appId, remoteExtensions)
+  }
+
+  if (flowExtensionsToMigrate.length > 0) {
+    const confirmedMigration = await extensionMigrationPrompt(flowExtensionsToMigrate, false)
+    if (!confirmedMigration) throw new AbortSilentError()
+    const newRemoteExtensions = await migrateFlowExtensions(
+      flowExtensionsToMigrate,
+      options.appId,
+      dashboardOnlyExtensions,
+    )
+    remoteExtensions = remoteExtensions.concat(newRemoteExtensions)
+  }
+
+  const matchExtensions = await automaticMatchmaking(localExtensions, remoteExtensions, validIdentifiers, 'uuid')
+
+  let validMatches = matchExtensions.identifiers
+  const extensionsToCreate = matchExtensions.toCreate ?? []
+
+  for (const pending of matchExtensions.toConfirm) {
+    // eslint-disable-next-line no-await-in-loop
+    const confirmed = await matchConfirmationPrompt(pending.local, pending.remote)
+    if (confirmed) {
+      validMatches[pending.local.localIdentifier] = pending.remote.uuid
+    } else {
+      extensionsToCreate.push(pending.local)
+    }
+  }
+
+  let onlyRemoteExtensions = matchExtensions.toManualMatch.remote ?? []
+
+  if (matchExtensions.toManualMatch.local.length > 0) {
+    const matchResult = await manualMatchIds(matchExtensions.toManualMatch, 'uuid')
+    validMatches = {...validMatches, ...matchResult.identifiers}
+    extensionsToCreate.push(...matchResult.toCreate)
+    onlyRemoteExtensions = matchResult.onlyRemote
+  }
+
+  return {
+    validMatches,
+    extensionsToCreate,
+    dashboardOnlyExtensions,
+  }
+}
+
+export async function deployConfirmed(
+  options: EnsureDeploymentIdsPresenceOptions,
+  extensionRegistrations: RemoteSource[],
+  configurationRegistrations: RemoteSource[],
+  {
+    validMatches,
+    extensionsToCreate,
+  }: {
+    validMatches: IdentifiersExtensions
+    extensionsToCreate: LocalSource[]
+  },
+) {
+  const {extensionsNonUuidManaged, extensionsIdsNonUuidManaged} = await ensureNonUuidManagedExtensionsIds(
+    configurationRegistrations,
+    options.app,
+    options.appId,
+  )
+
+  const validMatchesById: {[key: string]: string} = {}
+  if (extensionsToCreate.length > 0) {
+    const newIdentifiers = await createExtensions(extensionsToCreate, options.appId)
+    for (const [localIdentifier, registration] of Object.entries(newIdentifiers)) {
+      validMatches[localIdentifier] = registration.uuid
+      validMatchesById[localIdentifier] = registration.id
+    }
+  }
+
+  // For extensions we also need the match by ID, not only UUID (doesn't apply to functions)
+  for (const [localIdentifier, uuid] of Object.entries(validMatches)) {
+    const registration = extensionRegistrations.find((registration) => registration.uuid === uuid)
+    if (registration) validMatchesById[localIdentifier] = registration.id
+  }
+
+  return {
+    extensions: validMatches,
+    extensionIds: {...validMatchesById, ...extensionsIdsNonUuidManaged},
+    extensionsNonUuidManaged,
+  }
+}
+
+async function ensureNonUuidManagedExtensionsIds(
+  remoteConfigurationRegistrations: RemoteSource[],
+  app: AppInterface,
+  appId: string,
+) {
+  const localExtensionRegistrations = app.allExtensions.filter((ext) => !ext.isUuidManaged())
+  const extensionsToCreate: LocalSource[] = []
+  const validMatches: {[key: string]: string} = {}
+  const validMatchesById: {[key: string]: string} = {}
+  localExtensionRegistrations.forEach((local) => {
+    const possibleMatch = remoteConfigurationRegistrations.find((remote) => remote.type === local.graphQLType)
+    if (possibleMatch) {
+      validMatches[local.localIdentifier] = possibleMatch.uuid
+      validMatchesById[local.localIdentifier] = possibleMatch.id
+    } else extensionsToCreate.push(local)
+  })
+
+  if (extensionsToCreate.length > 0) {
+    const newIdentifiers = await createExtensions(extensionsToCreate, appId, false)
+    for (const [localIdentifier, registration] of Object.entries(newIdentifiers)) {
+      validMatches[localIdentifier] = registration.uuid
+      validMatchesById[localIdentifier] = registration.id
+    }
+  }
+
+  return {extensionsNonUuidManaged: validMatches, extensionsIdsNonUuidManaged: validMatchesById}
+}
+
+async function createExtensions(extensions: LocalSource[], appId: string, output = true) {
+  const token = await ensureAuthenticatedPartners()
+  const result: {[localIdentifier: string]: RemoteSource} = {}
+  for (const extension of extensions) {
+    // Create one at a time to avoid API rate limiting issues.
+    // eslint-disable-next-line no-await-in-loop
+    const registration = await createExtension(appId, extension.graphQLType, extension.handle, token)
+    if (output) outputCompleted(`Created extension ${extension.handle}.`)
+    result[extension.localIdentifier] = registration
+  }
+  return result
+}
