94aaab5cde12c69b0d04f37f96afd6a924e8f5de
drzo <d@rzo.io>
2024-05-15 16:53:26 +0200

    Initialize no-duplicated-flags

---
+++ packages/features/steps/commands.steps.ts
@@ -0,0 +1,53 @@
+import {executables} from '../lib/constants'
+import {exec} from '../lib/system'
+import {World} from '../world/index.js'
+import {When, Then} from '@cucumber/cucumber'
+import fg from 'fast-glob'
+import * as fs from 'fs/promises'
+import {strict as assert} from 'assert'
+
+When(/I import the commands/, async function (this: World) {
+  const oclifManifestPaths = await fg('packages/*/oclif.manifest.json', {cwd: this.rootDirectory, absolute: true})
+  const commands = (
+    await Promise.all(
+      oclifManifestPaths.map(async (oclifManifestPath) => {
+        const {commands} = JSON.parse(await (await fs.readFile(oclifManifestPath)).toString())
+        return commands
+      }),
+    )
+  ).flatMap((commands) => commands)
+  const commandsWithDuplicatedFlags: {command: string; flag: string}[] = []
+  commands.forEach((command) => {
+    const flags = Object.entries(command.flags).map()
+  })
+})
+
+const errorMessage = `
+SNAPSHOT TEST FAILED!
+
+The result of 'yarn shopify commands' has changed! We run this to check that
+all commands can load successfully.
+
+It's normal to see this test fail when you add or remove a command in the CLI.
+In this case you can run this command to regenerate the snapshot file:
+
+$ pnpm test:regenerate-snapshots
+
+Then you can commit this change and this test will pass.
+
+If instead you didn't mean to change a command, UH OH. Check the commands in
+the diff below and figure out what is broken.
+`
+
+When(/I list the available commands/, async function () {
+  const commandFlags = ['commands', '--no-header', '--columns=Command,Plugin']
+  this.commandResult = (
+    await exec('node', [executables.cli, ...commandFlags], {env: {...process.env, ...this.temporaryEnv}})
+  ).stdout
+})
+
+Then(/I see all commands matching the snapshot/, async function () {
+  const snapshot: string = await fs.readFile('snapshots/commands.txt', {encoding: 'utf8'})
+  const normalize = (value: string) => value.replace(/\r\n/g, '\n').trimEnd()
+  assert.equal(normalize(snapshot), normalize(this.commandResult), errorMessage)
+})
---
+++ packages/features/world/index.ts
@@ -0,0 +1,32 @@
+import {executables} from '../lib/constants'
+import {exec} from '../lib/system'
+import {setWorldConstructor} from '@cucumber/cucumber'
+import {join as joinPath} from 'pathe'
+
+export interface WorldConstructorParams {
+  temporaryDirectory: string
+}
+
+export class World {
+  public temporaryDirectory: string
+  public temporaryEnv: {[key: string]: string} | undefined
+  public appDirectory: string | undefined
+  public rootDirectory: string
+
+  constructor({temporaryDirectory}: WorldConstructorParams) {
+    this.temporaryDirectory = temporaryDirectory
+    this.rootDirectory = joinPath(__dirname, '../../../')
+  }
+
+  public async appInfo() {
+    if (!this.appDirectory) {
+      throw new Error("An app hasn't been created. Make sure the acceptance test has a step to create an app.")
+    }
+    const {stdout} = await exec('node', [executables.cli, 'app', 'info', '--path', this.appDirectory, '--json'], {
+      env: {...process.env, ...this.temporaryEnv},
+    })
+    return JSON.parse(stdout)
+  }
+}
+
+setWorldConstructor(World)
