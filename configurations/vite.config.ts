2e71ce5b1e10ce6f14e7c4c25c786f27d2e3efc6
drzo <d@rzo.io>
2024-05-15 16:48:40 +0200

    Initialize order-details-ui-extension-prototype

---
+++ configurations/vite.config.ts
@@ -0,0 +1,41 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import path from 'pathe'
+import {defineConfig} from 'vite'
+
+export default function config(packagePath: string) {
+  return defineConfig({
+    resolve: {
+      alias: aliases(packagePath),
+    },
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    test: {
+      clearMocks: true,
+      mockReset: true,
+      setupFiles: [path.join(__dirname, './vitest/setup.js')],
+    },
+  })
+}
+
+export const aliases = (packagePath: string) => {
+  return [
+    {
+      find: /@shopify\/cli-kit\/(.+)/,
+      replacement: (importedModule: string) => {
+        const migratedTsModules = ['array']
+        const migratedTsxModules = ['ui']
+        if (migratedTsModules.find((module) => importedModule.endsWith(module))) {
+          return path.join(packagePath, `../cli-kit/src/public/${importedModule.replace('@shopify/cli-kit/', '')}.ts`)
+        } else if (migratedTsxModules.find((module) => importedModule.endsWith(module))) {
+          return path.join(packagePath, `../cli-kit/src/public/${importedModule.replace('@shopify/cli-kit/', '')}.tsx`)
+        } else {
+          return path.join(packagePath, `../cli-kit/src/${importedModule.replace('@shopify/cli-kit/', '')}.ts`)
+        }
+      },
+    },
+    {find: '@shopify/cli-kit', replacement: path.join(packagePath, '../cli-kit/src/index.ts')},
+  ]
+}
