fc79a675b637026d7c441e2bec29fdc8bcba6efc
drzo <d@rzo.io>
2024-05-15 16:50:43 +0200

    Initialize remove-APIClient

---
+++ packages/cli-kit/src/private/node/ui/components/Link.tsx
@@ -0,0 +1,22 @@
+import chalk from 'chalk'
+import {Text} from 'ink'
+import React from 'react'
+import terminalLink from 'terminal-link'
+
+interface Props {
+  url: string
+  label?: string
+}
+
+function fallback(text: string, url: string) {
+  return `${text} ${chalk.dim(`(${url})`)}`
+}
+
+/**
+ * `Link` displays a clickable link when supported by the terminal.
+ */
+const Link: React.FC<Props> = ({url, label}: React.PropsWithChildren<Props>): JSX.Element => {
+  return <Text>{terminalLink(label ?? url, url, {fallback: label ? fallback : false})}</Text>
+}
+
+export {Link}
