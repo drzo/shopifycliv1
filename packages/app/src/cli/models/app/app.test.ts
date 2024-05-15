64e64509f7e6b1bf1eedec10d829f6c0bd718a54
drzo <d@rzo.io>
2024-05-15 17:04:39 +0200

    Initialize exclude-config-and-api-key-flags

---
+++ packages/app/src/cli/models/app/app.test.ts
@@ -0,0 +1,180 @@
+import {CurrentAppConfiguration, getUIExtensionRendererVersion, isCurrentAppSchema, isLegacyAppSchema} from './app.js'
+import {testApp, testUIExtension} from './app.test-data.js'
+import {describe, expect, test} from 'vitest'
+import {inTemporaryDirectory, mkdir, writeFile} from '@shopify/cli-kit/node/fs'
+import {joinPath} from '@shopify/cli-kit/node/path'
+
+const DEFAULT_APP = testApp()
+
+const CORRECT_CURRENT_APP_SCHEMA: CurrentAppConfiguration = {
+  name: 'app 1',
+  api_contact_email: 'ryan@shopify.com',
+  client_id: '12345',
+  webhooks: {
+    api_version: '2023-04',
+    privacy_compliance: {
+      customer_deletion_url: 'https://google.com/',
+      customer_data_request_url: 'https://google.com/',
+      shop_deletion_url: 'https://google.com/',
+    },
+  },
+  application_url: 'http://example.com',
+  embedded: true,
+  auth: {
+    redirect_urls: ['https://google.com'],
+  },
+  app_proxy: {
+    url: 'https://google.com/',
+    subpath: 'https://google.com/',
+    prefix: 'https://google.com/',
+  },
+  pos: {
+    embedded: false,
+  },
+  app_preferences: {
+    url: 'https://google.com/',
+  },
+  build: {
+    automatically_update_urls_on_dev: true,
+    dev_store_url: 'https://google.com/',
+  },
+}
+
+const CORRECT_LEGACY_APP_SCHEMA = {
+  extension_directories: [],
+  web_directories: [],
+  scopes: 'write_products',
+}
+
+describe('app schema validation', () => {
+  describe('legacy schema validator', () => {
+    test('checks whether legacy app schema is valid -- pass', () => {
+      expect(isLegacyAppSchema(CORRECT_LEGACY_APP_SCHEMA)).toBe(true)
+    })
+    test('checks whether legacy app schema is valid -- fail', () => {
+      const config = {
+        ...CORRECT_LEGACY_APP_SCHEMA,
+        some_other_key: 'i am not valid, i will fail',
+      }
+      expect(isLegacyAppSchema(config)).toBe(false)
+    })
+  })
+
+  describe('current schema validator', () => {
+    test('checks whether current app schema is valid -- pass', () => {
+      expect(isCurrentAppSchema(CORRECT_CURRENT_APP_SCHEMA)).toBe(true)
+    })
+    test('checks whether current app schema is valid -- fail', () => {
+      const config = {
+        ...CORRECT_CURRENT_APP_SCHEMA,
+        bad_key: 'i will fail',
+      }
+      expect(isCurrentAppSchema(config)).toBe(false)
+    })
+    test('checks whether current app schema is valid -- fail', () => {
+      const config = {
+        ...CORRECT_CURRENT_APP_SCHEMA,
+      }
+
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      delete config.name
+
+      expect(isCurrentAppSchema(config)).toBe(false)
+    })
+  })
+})
+
+describe('getUIExtensionRendererVersion', () => {
+  test('returns the version of the dependency package for product_subscription', async () => {
+    await inTemporaryDirectory(async (tmpDir) => {
+      // Given
+      await createPackageJson(tmpDir, 'admin-ui-extensions', '2.4.5')
+      DEFAULT_APP.directory = tmpDir
+      const extension = await testUIExtension({type: 'product_subscription'})
+
+      // When
+      const got = await getUIExtensionRendererVersion(extension, DEFAULT_APP)
+
+      // Then
+      expect(got).not.toEqual('not-found')
+      if (got === 'not_found') return
+      expect(got?.name).to.toEqual('@shopify/admin-ui-extensions')
+      expect(got?.version).toEqual('2.4.5')
+    })
+  })
+
+  test('returns the version of the dependency package for checkout_ui_extension', async () => {
+    await inTemporaryDirectory(async (tmpDir) => {
+      // Given
+      await createPackageJson(tmpDir, 'checkout-ui-extensions', '1.4.5')
+      DEFAULT_APP.directory = tmpDir
+      const extension = await testUIExtension({type: 'checkout_ui_extension'})
+
+      // When
+      const got = await getUIExtensionRendererVersion(extension, DEFAULT_APP)
+
+      // Then
+      expect(got).not.toEqual('not-found')
+      if (got === 'not_found') return
+      expect(got?.name).to.toEqual('@shopify/checkout-ui-extensions')
+      expect(got?.version).toEqual('1.4.5')
+    })
+  })
+
+  test('returns the version of the dependency package for checkout_post_purchase', async () => {
+    await inTemporaryDirectory(async (tmpDir) => {
+      // Given
+      await createPackageJson(tmpDir, 'post-purchase-ui-extensions', '3.4.5')
+      DEFAULT_APP.directory = tmpDir
+      const extension = await testUIExtension({type: 'checkout_post_purchase'})
+
+      // When
+      const got = await getUIExtensionRendererVersion(extension, DEFAULT_APP)
+
+      // Then
+      expect(got).not.toEqual('not-found')
+      if (got === 'not_found') return
+      expect(got?.name).to.toEqual('@shopify/post-purchase-ui-extensions')
+      expect(got?.version).toEqual('3.4.5')
+    })
+  })
+
+  test('returns the version of the dependency package for web_pixel', async () => {
+    await inTemporaryDirectory(async (tmpDir) => {
+      // Given
+      await createPackageJson(tmpDir, '@shopify/web-pixels-extension', '3.4.5')
+      DEFAULT_APP.directory = tmpDir
+      const extension = await testUIExtension({type: 'web_pixel_extension'})
+
+      // When
+      const got = await getUIExtensionRendererVersion(extension, DEFAULT_APP)
+
+      // Then
+      expect(got).not.toEqual('not-found')
+      if (got === 'not_found') return
+      expect(got?.name).to.toEqual('@shopify/web-pixels-extension')
+      expect(got?.version).toEqual('3.4.5')
+    })
+  })
+
+  test('returns not_found if there is no renderer package', async () => {
+    await inTemporaryDirectory(async (tmpDir) => {
+      DEFAULT_APP.directory = tmpDir
+      const extension = await testUIExtension({type: 'product_subscription'})
+
+      // When
+      const got = await getUIExtensionRendererVersion(extension, DEFAULT_APP)
+
+      // Then
+      expect(got).toEqual('not_found')
+    })
+  })
+})
+
+function createPackageJson(tmpDir: string, type: string, version: string) {
+  const packagePath = joinPath(tmpDir, 'node_modules', '@shopify', type, 'package.json')
+  const packageJson = {name: 'name', version}
+  const dirPath = joinPath(tmpDir, 'node_modules', '@shopify', type)
+  return mkdir(dirPath).then(() => writeFile(packagePath, JSON.stringify(packageJson)))
+}
---
+++ packages/app/src/cli/services/app/config/link.test.ts
@@ -0,0 +1,326 @@
+import link, {LinkOptions} from './link.js'
+import {saveCurrentConfig} from './use.js'
+import {testApp, testOrganizationApp} from '../../../models/app/app.test-data.js'
+import {selectConfigName} from '../../../prompts/config.js'
+import {loadApp} from '../../../models/app/loader.js'
+import {fetchOrCreateOrganizationApp} from '../../context.js'
+import {fetchAppFromApiKey} from '../../dev/fetch.js'
+import {describe, expect, test, vi} from 'vitest'
+import {Config} from '@oclif/core'
+import {fileExistsSync, inTemporaryDirectory, readFile, writeFileSync} from '@shopify/cli-kit/node/fs'
+import {joinPath} from '@shopify/cli-kit/node/path'
+import {renderSuccess} from '@shopify/cli-kit/node/ui'
+import {ensureAuthenticatedPartners} from '@shopify/cli-kit/node/session'
+
+const LOCAL_APP = testApp()
+const REMOTE_APP = testOrganizationApp()
+
+vi.mock('./use.js')
+vi.mock('../../../prompts/config.js')
+vi.mock('../../../models/app/loader.js', async () => {
+  const loader: any = await vi.importActual('../../../models/app/loader.js')
+  return {
+    ...loader,
+    loadApp: vi.fn(),
+  }
+})
+vi.mock('@shopify/cli-kit/node/ui')
+vi.mock('@shopify/cli-kit/node/session')
+vi.mock('../../dev/fetch.js')
+vi.mock('../../context.js', async () => {
+  const context: any = await vi.importActual('../../context.js')
+  return {
+    ...context,
+    fetchOrCreateOrganizationApp: vi.fn(),
+  }
+})
+
+describe('link', () => {
+  test('does not ask for a name when it is provided as a flag', async () => {
+    await inTemporaryDirectory(async (tmp) => {
+      // Given
+      const options: LinkOptions = {
+        directory: tmp,
+        commandConfig: {runHook: vi.fn(() => Promise.resolve({successes: []}))} as unknown as Config,
+        configName: 'Default value',
+      }
+      vi.mocked(loadApp).mockResolvedValue(LOCAL_APP)
+      vi.mocked(fetchOrCreateOrganizationApp).mockResolvedValue(REMOTE_APP)
+
+      // When
+      await link(options)
+
+      // Then
+      expect(selectConfigName).not.toHaveBeenCalled()
+      expect(fileExistsSync(joinPath(tmp, 'shopify.app.default-value.toml'))).toBeTruthy()
+    })
+  })
+
+  test('creates a new shopify.app.toml file when it does not exist', async () => {
+    await inTemporaryDirectory(async (tmp) => {
+      // Given
+      const options: LinkOptions = {
+        directory: tmp,
+        commandConfig: {runHook: vi.fn(() => Promise.resolve({successes: []}))} as unknown as Config,
+      }
+      vi.mocked(loadApp).mockResolvedValue(LOCAL_APP)
+      vi.mocked(fetchOrCreateOrganizationApp).mockResolvedValue(REMOTE_APP)
+
+      // When
+      await link(options)
+
+      // Then
+      const content = await readFile(joinPath(tmp, 'shopify.app.toml'))
+      const expectedContent = `client_id = "api-key"
+name = "app1"
+api_contact_email = "example@example.com"
+application_url = "https://example.com"
+embedded = true
+extension_directories = [ ]
+
+[webhooks]
+api_version = "2023-07"
+
+[auth]
+redirect_urls = [ "https://example.com/callback1" ]
+
+[pos]
+embedded = false
+
+[access_scopes]
+use_legacy_install_flow = true
+`
+      expect(content).toEqual(expectedContent)
+      expect(saveCurrentConfig).toHaveBeenCalledWith({configFileName: 'shopify.app.toml', directory: tmp})
+      expect(renderSuccess).toHaveBeenCalledWith({
+        headline: 'App "app1" connected to this codebase, file shopify.app.toml created',
+      })
+    })
+  })
+
+  test('creates a new shopify.app.staging.toml file when shopify.app.toml already linked', async () => {
+    await inTemporaryDirectory(async (tmp) => {
+      // Given
+      const options: LinkOptions = {
+        directory: tmp,
+        commandConfig: {runHook: vi.fn(() => Promise.resolve({successes: []}))} as unknown as Config,
+      }
+      vi.mocked(loadApp).mockResolvedValue(
+        testApp({
+          configurationPath: 'shopify.app.development.toml',
+          configuration: {
+            name: 'my app',
+            api_contact_email: 'example@example.com',
+            client_id: '12345',
+            scopes: 'write_products',
+            webhooks: {api_version: '2023-04'},
+            application_url: 'https://myapp.com',
+          },
+        }),
+      )
+      vi.mocked(fetchOrCreateOrganizationApp).mockResolvedValue(
+        testOrganizationApp({
+          apiKey: '12345',
+          applicationUrl: 'https://myapp.com',
+          title: 'my app',
+          requestedAccessScopes: ['write_products'],
+        }),
+      )
+      vi.mocked(selectConfigName).mockResolvedValue('staging')
+
+      // When
+      await link(options)
+
+      // Then
+      const content = await readFile(joinPath(tmp, 'shopify.app.staging.toml'))
+      const expectedContent = `client_id = "12345"
+name = "my app"
+api_contact_email = "example@example.com"
+application_url = "https://myapp.com"
+embedded = true
+
+[webhooks]
+api_version = "2023-07"
+
+[auth]
+redirect_urls = [ "https://example.com/callback1" ]
+
+[pos]
+embedded = false
+
+[access_scopes]
+scopes = "write_products"
+`
+      expect(content).toEqual(expectedContent)
+      expect(saveCurrentConfig).toHaveBeenCalledWith({configFileName: 'shopify.app.staging.toml', directory: tmp})
+      expect(renderSuccess).toHaveBeenCalledWith({
+        headline: 'App "my app" connected to this codebase, file shopify.app.staging.toml created',
+      })
+    })
+  })
+
+  test('updates the shopify.app.toml when it already exists and is unlinked', async () => {
+    await inTemporaryDirectory(async (tmp) => {
+      // Given
+      const filePath = joinPath(tmp, 'shopify.app.toml')
+      const initialContent = `scopes = ""
+      `
+      writeFileSync(filePath, initialContent)
+      const options: LinkOptions = {
+        directory: tmp,
+        commandConfig: {runHook: vi.fn(() => Promise.resolve({successes: []}))} as unknown as Config,
+      }
+      vi.mocked(loadApp).mockResolvedValue(LOCAL_APP)
+      vi.mocked(fetchOrCreateOrganizationApp).mockResolvedValue(REMOTE_APP)
+
+      // When
+      await link(options)
+
+      // Then
+      const content = await readFile(joinPath(tmp, 'shopify.app.toml'))
+      const expectedContent = `client_id = "api-key"
+name = "app1"
+api_contact_email = "example@example.com"
+application_url = "https://example.com"
+embedded = true
+extension_directories = [ ]
+
+[webhooks]
+api_version = "2023-07"
+
+[auth]
+redirect_urls = [ "https://example.com/callback1" ]
+
+[pos]
+embedded = false
+
+[access_scopes]
+use_legacy_install_flow = true
+`
+      expect(content).toEqual(expectedContent)
+      expect(renderSuccess).toHaveBeenCalledWith({
+        headline: 'App "app1" connected to this codebase, file shopify.app.toml updated',
+      })
+    })
+  })
+
+  test('fetches the app directly when an api key is provided', async () => {
+    await inTemporaryDirectory(async (tmp) => {
+      // Given
+      const options: LinkOptions = {
+        directory: tmp,
+        commandConfig: {runHook: vi.fn(() => Promise.resolve({successes: []}))} as unknown as Config,
+        apiKey: 'api-key',
+      }
+      vi.mocked(loadApp).mockResolvedValue(LOCAL_APP)
+      vi.mocked(ensureAuthenticatedPartners).mockResolvedValue('token')
+      vi.mocked(fetchAppFromApiKey).mockResolvedValue(REMOTE_APP)
+      vi.mocked(selectConfigName).mockResolvedValue('staging')
+
+      // When
+      await link(options)
+
+      // Then
+      expect(fetchAppFromApiKey).toHaveBeenCalledWith('api-key', 'token')
+    })
+  })
+
+  test('throws an error when an invalid api key is is provided', async () => {
+    await inTemporaryDirectory(async (tmp) => {
+      // Given
+      const options: LinkOptions = {
+        directory: tmp,
+        commandConfig: {runHook: vi.fn(() => Promise.resolve({successes: []}))} as unknown as Config,
+        apiKey: '1234-5678',
+      }
+      vi.mocked(loadApp).mockResolvedValue(LOCAL_APP)
+      vi.mocked(ensureAuthenticatedPartners).mockResolvedValue('token')
+      vi.mocked(fetchAppFromApiKey).mockResolvedValue(undefined)
+      vi.mocked(selectConfigName).mockResolvedValue('staging')
+
+      // When
+      const result = link(options)
+
+      // Then
+      await expect(result).rejects.toThrow(/Invalid Client ID/)
+    })
+  })
+
+  test('generates the file when there is no shopify.app.toml', async () => {
+    await inTemporaryDirectory(async (tmp) => {
+      // Given
+      const options: LinkOptions = {
+        directory: tmp,
+        commandConfig: {runHook: vi.fn(() => Promise.resolve({successes: []}))} as unknown as Config,
+      }
+      vi.mocked(loadApp).mockRejectedValue(new Error('Shopify.app.toml not found'))
+      vi.mocked(fetchOrCreateOrganizationApp).mockResolvedValue(REMOTE_APP)
+
+      // When
+      await link(options)
+
+      // Then
+      const content = await readFile(joinPath(tmp, 'shopify.app.toml'))
+      const expectedContent = `client_id = "api-key"
+name = "app1"
+api_contact_email = "example@example.com"
+application_url = "https://example.com"
+embedded = true
+
+[webhooks]
+api_version = "2023-07"
+
+[auth]
+redirect_urls = [ "https://example.com/callback1" ]
+
+[pos]
+embedded = false
+
+[access_scopes]
+use_legacy_install_flow = true
+`
+      expect(content).toEqual(expectedContent)
+    })
+  })
+
+  test('uses scopes on platform if defined', async () => {
+    await inTemporaryDirectory(async (tmp) => {
+      // Given
+      const options: LinkOptions = {
+        directory: tmp,
+        commandConfig: {runHook: vi.fn(() => Promise.resolve({successes: []}))} as unknown as Config,
+      }
+      vi.mocked(loadApp).mockResolvedValue(LOCAL_APP)
+      vi.mocked(fetchOrCreateOrganizationApp).mockResolvedValue({
+        ...REMOTE_APP,
+        requestedAccessScopes: ['read_products', 'write_orders'],
+      })
+
+      // When
+      await link(options)
+
+      // Then
+      const content = await readFile(joinPath(tmp, 'shopify.app.toml'))
+      const expectedContent = `client_id = "api-key"
+name = "app1"
+api_contact_email = "example@example.com"
+application_url = "https://example.com"
+embedded = true
+extension_directories = [ ]
+
+[webhooks]
+api_version = "2023-07"
+
+[auth]
+redirect_urls = [ "https://example.com/callback1" ]
+
+[pos]
+embedded = false
+
+[access_scopes]
+scopes = "read_products,write_orders"
+`
+      expect(content).toEqual(expectedContent)
+    })
+  })
+})
