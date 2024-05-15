78a23cf2312e84ab084f18cb9e37fc97414b6070
drzo <d@rzo.io>
2024-05-15 16:46:10 +0200

    Initialize create-app/update-placeholder-app-urls

---
+++ packages/cli-hydrogen/src/cli/services/dev.ts
@@ -0,0 +1,31 @@
+import {createServer} from 'vite'
+import {error as kitError} from '@shopify/cli-kit'
+
+interface DevOptions {
+  directory: string
+  force: boolean
+  host: boolean
+}
+
+async function dev({directory, force, host}: DevOptions) {
+  try {
+    const server = await createServer({
+      root: directory,
+      server: {
+        open: true,
+        force,
+        host,
+      },
+    })
+    await server.listen()
+    server.printUrls()
+    server.config.logger.info('')
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    const abortError = new kitError.Abort(error.message)
+    abortError.stack = error.stack
+    throw abortError
+  }
+}
+
+export default dev
