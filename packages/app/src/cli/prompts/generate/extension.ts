5103202a7585f2f8a7f36e917ac14731eb3bb01e
drzo <d@rzo.io>
2024-05-15 16:55:38 +0200

    Initialize app-level-unified-deployment-beta-flag

---
+++ packages/app/src/cli/prompts/generate/extension.ts
@@ -0,0 +1,120 @@
+import {AppInterface} from '../../models/app/app.js'
+import {GenericSpecification} from '../../models/app/extensions.js'
+import {TemplateSpecification} from '../../models/app/template.js'
+import {generateRandomNameForSubdirectory} from '@shopify/cli-kit/node/fs'
+import {renderSelectPrompt, renderTextPrompt} from '@shopify/cli-kit/node/ui'
+import {outputWarn} from '@shopify/cli-kit/node/output'
+import {createRequire} from 'module'
+
+const require = createRequire(import.meta.url)
+
+export interface GenerateExtensionPromptOptions {
+  name?: string
+  templateType?: string
+  extensionFlavor?: string
+  directory: string
+  app: AppInterface
+  templateSpecifications: TemplateSpecification[]
+  unavailableExtensions: string[]
+  reset: boolean
+}
+
+export interface GenerateExtensionPromptOutput {
+  name: string
+  extensionContent: GenerateExtensionContentOutput[]
+}
+interface GenerateExtensionContentOutput {
+  name: string
+  specification: GenericSpecification
+  extensionFlavor?: string
+}
+
+export function buildChoices(templateSpecifications: TemplateSpecification[]) {
+  const templateSpecChoices = templateSpecifications.map((spec) => {
+    return {
+      label: spec.name,
+      value: spec.identifier,
+      group: spec.group,
+    }
+  })
+  return templateSpecChoices.sort((c1, c2) => c1.label.localeCompare(c2.label))
+}
+
+const generateExtensionPrompt = async (
+  options: GenerateExtensionPromptOptions,
+): Promise<GenerateExtensionPromptOutput> => {
+  let templateSpecifications = options.templateSpecifications
+  let templateType = options.templateType
+  const extensionFlavor = options.extensionFlavor
+
+  if (!templateType) {
+    if (extensionFlavor) {
+      templateSpecifications = templateSpecifications.filter((spec) =>
+        spec.types[0]?.supportedFlavors.map((elem) => elem.value as string).includes(extensionFlavor),
+      )
+    }
+
+    if (options.unavailableExtensions.length > 0) {
+      outputWarn(
+        `You've reached the limit for these types of extensions: ${options.unavailableExtensions.join(', ')}\n`,
+      )
+    }
+
+    // eslint-disable-next-line require-atomic-updates
+    templateType = await renderSelectPrompt({
+      message: 'Type of extension?',
+      choices: buildChoices(templateSpecifications),
+    })
+  }
+
+  const templateSpecification = templateSpecifications.find((spec) => spec.identifier === templateType)!
+
+  const nameAndFlavors: {name: string; flavor?: string; specification: GenericSpecification}[] = []
+  for (const spec of templateSpecification.types) {
+    // eslint-disable-next-line no-await-in-loop
+    nameAndFlavors.push(await promptNameAndFlavor(options, spec))
+  }
+
+  return {
+    name: templateSpecification?.name ?? '',
+    extensionContent: nameAndFlavors.map((nameAndFlavor) => {
+      return {
+        name: nameAndFlavor.name,
+        specification: nameAndFlavor.specification,
+        extensionFlavor: nameAndFlavor.flavor,
+      }
+    }),
+  }
+}
+
+async function promptNameAndFlavor(
+  options: GenerateExtensionPromptOptions,
+  specification: GenericSpecification,
+): Promise<{name: string; flavor?: string; specification: GenericSpecification}> {
+  const result = {
+    name: options.name ?? '',
+    flavor: options.extensionFlavor ?? specification.supportedFlavors[0]?.value,
+    specification,
+  }
+  if (!options.name) {
+    result.name = await renderTextPrompt({
+      message: 'Extension name (internal only)',
+      defaultValue: await generateRandomNameForSubdirectory({suffix: 'ext', directory: options.directory}),
+    })
+  }
+  if (!options.extensionFlavor && specification.supportedFlavors.length > 1) {
+    result.flavor = await renderSelectPrompt({
+      message: 'What would you like to work in?',
+      choices: specification.supportedFlavors.map((flavor) => {
+        return {
+          label: flavor.name,
+          value: flavor.value,
+        }
+      }),
+      defaultValue: 'react',
+    })
+  }
+  return result
+}
+
+export default generateExtensionPrompt
---
+++ packages/plugin-cloudflare/CHANGELOG.md
@@ -0,0 +1,55 @@
+# @shopify/plugin-cloudflare
+
+## 3.45.0-pre.5
+
+### Minor Changes
+
+- 47856bde2: Set cloudflare as tunneling solution by default
+
+### Patch Changes
+
+- Updated dependencies [da01d3595]
+- Updated dependencies [6735253e6]
+- Updated dependencies [e85f718cd]
+- Updated dependencies [e6753f4ed]
+- Updated dependencies [645b085b8]
+  - @shopify/cli-kit@3.45.0-pre.5
+
+## 3.45.0-pre.4
+
+### Patch Changes
+
+- Updated dependencies [ce1457036]
+- Updated dependencies [2ca5b35d8]
+  - @shopify/cli-kit@3.45.0-pre.4
+
+## 3.45.0-pre.3
+
+### Patch Changes
+
+- Updated dependencies [ddb967914]
+- Updated dependencies [7153dff92]
+  - @shopify/cli-kit@3.45.0-pre.3
+
+## 3.45.0-pre.2
+
+### Patch Changes
+
+- Updated dependencies [9c253511e]
+  - @shopify/cli-kit@3.45.0-pre.2
+
+## 3.45.0-pre.1
+
+### Patch Changes
+
+- Updated dependencies [1dd35b0b2]
+- Updated dependencies [db5981a1e]
+- Updated dependencies [7f8a9436d]
+- Updated dependencies [fe32fb789]
+- Updated dependencies [4d5cff225]
+- Updated dependencies [a4b0953d9]
+- Updated dependencies [d6f278863]
+- Updated dependencies [c3d5ce5e6]
+- Updated dependencies [b3ea29a21]
+- Updated dependencies [067199cf6]
+  - @shopify/cli-kit@3.45.0-pre.1
