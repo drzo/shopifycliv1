62704b494dc613463b849b77628e3308eae69306
drzo <d@rzo.io>
2024-05-15 17:09:50 +0200

    Initialize montalvomiguelo-notify-timing-fix

---
+++ packages/app/src/cli/services/app/env/show.test.ts
@@ -0,0 +1,72 @@
+import {showEnv} from './show.js'
+import {fetchOrgAndApps, fetchOrganizations} from '../../dev/fetch.js'
+import {fetchAppFromConfigOrSelect} from '../fetch-app-from-config-or-select.js'
+import {AppInterface} from '../../../models/app/app.js'
+import {selectOrganizationPrompt} from '../../../prompts/dev.js'
+import {testApp, testOrganizationApp} from '../../../models/app/app.test-data.js'
+import {describe, expect, vi, test} from 'vitest'
+import {ensureAuthenticatedPartners} from '@shopify/cli-kit/node/session'
+import * as file from '@shopify/cli-kit/node/fs'
+import {stringifyMessage, unstyled} from '@shopify/cli-kit/node/output'
+import {joinPath} from '@shopify/cli-kit/node/path'
+
+vi.mock('../../dev/fetch.js')
+vi.mock('../fetch-app-from-config-or-select.js')
+vi.mock('../../../prompts/dev.js')
+vi.mock('@shopify/cli-kit/node/session')
+vi.mock('@shopify/cli-kit/node/node-package-manager')
+
+describe('env show', () => {
+  test('outputs the new environment', async () => {
+    // Given
+    vi.spyOn(file, 'writeFile')
+
+    const app = mockApp()
+    const token = 'token'
+    const organization = {
+      id: '123',
+      betas: {},
+      businessName: 'test',
+      website: '',
+      apps: {nodes: []},
+    }
+    const organizationApp = testOrganizationApp()
+
+    vi.mocked(fetchOrganizations).mockResolvedValue([organization])
+    vi.mocked(selectOrganizationPrompt).mockResolvedValue(organization)
+    vi.mocked(fetchOrgAndApps).mockResolvedValue({
+      organization,
+      stores: [],
+      apps: {nodes: [organizationApp], pageInfo: {hasNextPage: false}},
+    })
+    vi.mocked(fetchAppFromConfigOrSelect).mockResolvedValue(organizationApp)
+    vi.mocked(ensureAuthenticatedPartners).mockResolvedValue(token)
+
+    // When
+    const result = await showEnv(app)
+
+    // Then
+    expect(file.writeFile).not.toHaveBeenCalled()
+    expect(unstyled(stringifyMessage(result))).toMatchInlineSnapshot(`
+    "
+        SHOPIFY_API_KEY=api-key
+        SHOPIFY_API_SECRET=api-secret
+        SCOPES=my-scope
+      "
+    `)
+  })
+})
+
+function mockApp(currentVersion = '2.2.2'): AppInterface {
+  const nodeDependencies: {[key: string]: string} = {}
+  nodeDependencies['@shopify/cli'] = currentVersion
+  return testApp({
+    name: 'myapp',
+    directory: '/',
+    configuration: {
+      path: joinPath('/', 'shopify.app.toml'),
+      scopes: 'my-scope',
+    },
+    nodeDependencies,
+  })
+}
---
+++ packages/cli-kit/src/private/node/api/graphql.ts
@@ -0,0 +1,50 @@
+import {GraphQLClientError, sanitizedHeadersOutput} from './headers.js'
+import {stringifyMessage, outputContent, outputToken, outputDebug} from '../../../public/node/output.js'
+import {AbortError} from '../../../public/node/error.js'
+import {ClientError, RequestDocument, Variables} from 'graphql-request'
+
+export function debugLogRequestInfo<T>(
+  api: string,
+  query: RequestDocument,
+  variables?: Variables,
+  headers: {[key: string]: string} = {},
+) {
+  outputDebug(outputContent`Sending ${outputToken.json(api)} GraphQL request:
+  ${outputToken.raw(query.toString().trim())}
+${variables ? `\nWith variables:\n${sanitizeVariables(variables)}\n` : ''}
+With request headers:
+${sanitizedHeadersOutput(headers)}
+`)
+}
+
+function sanitizeVariables(variables: Variables): string {
+  const result: Variables = {...variables}
+  if ('apiKey' in result) {
+    result.apiKey = '*****'
+  }
+  return JSON.stringify(result, null, 2)
+}
+
+export function errorHandler<T>(api: string): (error: unknown) => Error | unknown {
+  return (error: unknown) => {
+    if (error instanceof ClientError) {
+      const errorMessage = stringifyMessage(outputContent`
+  The ${outputToken.raw(
+    api,
+  )} GraphQL API responded unsuccessfully with the HTTP status ${`${error.response.status}`} and errors:
+
+  ${outputToken.json(error.response.errors)}
+      `)
+      let mappedError: Error
+      if (error.response.status < 500) {
+        mappedError = new GraphQLClientError(errorMessage, error.response.status, error.response.errors)
+      } else {
+        mappedError = new AbortError(errorMessage)
+      }
+      mappedError.stack = error.stack
+      return mappedError
+    } else {
+      return error
+    }
+  }
+}
