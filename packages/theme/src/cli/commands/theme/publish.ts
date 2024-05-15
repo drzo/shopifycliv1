8fac9684c92705514d890ed06277f8dda5e644da
drzo <d@rzo.io>
2024-05-15 16:47:29 +0200

    Initialize js.remove-unused-query

---
+++ packages/theme/src/cli/commands/theme/publish.ts
@@ -0,0 +1,41 @@
+import {getTheme} from '../../utilities/theme-store.js'
+import ThemeCommand from '../../utilities/theme-command.js'
+import {Flags} from '@oclif/core'
+import {cli, session, string} from '@shopify/cli-kit'
+import {execCLI2} from '@shopify/cli-kit/node/ruby'
+
+export default class Publish extends ThemeCommand {
+  static description = 'Set a remote theme as the live theme.'
+
+  static args = [{name: 'themeId', description: 'The ID of the theme', required: false}]
+
+  static flags = {
+    ...cli.globalFlags,
+    force: Flags.boolean({
+      char: 'f',
+      description: 'Skip confirmation.',
+      env: 'SHOPIFY_FLAG_FORCE',
+    }),
+    store: Flags.string({
+      char: 's',
+      description: 'Store URL',
+      env: 'SHOPIFY_FLAG_STORE',
+      parse: (input, _) => Promise.resolve(string.normalizeStoreName(input)),
+    }),
+  }
+
+  async run(): Promise<void> {
+    const {flags, args} = await this.parse(Publish)
+
+    const store = getTheme(flags)
+    const flagsToPass = this.passThroughFlags(flags, {exclude: ['path', 'store', 'verbose']})
+    const command = ['theme', 'publish']
+    if (args.themeId) {
+      command.push(args.themeId)
+    }
+    command.push(...flagsToPass)
+
+    const adminSession = await session.ensureAuthenticatedAdmin(store)
+    await execCLI2(command, {adminSession})
+  }
+}
---
+++ packages/theme/src/cli/commands/theme/pull.ts
@@ -0,0 +1,70 @@
+import {themeFlags} from '../../flags.js'
+import {getTheme} from '../../utilities/theme-store.js'
+import ThemeCommand from '../../utilities/theme-command.js'
+import {Flags} from '@oclif/core'
+import {cli, path, session, string} from '@shopify/cli-kit'
+import {execCLI2} from '@shopify/cli-kit/node/ruby'
+
+export default class Pull extends ThemeCommand {
+  static description = 'Download your remote theme files locally.'
+
+  static flags = {
+    ...cli.globalFlags,
+    ...themeFlags,
+    theme: Flags.string({
+      char: 't',
+      description: 'Theme ID or name of the remote theme.',
+      env: 'SHOPIFY_FLAG_THEME_ID',
+    }),
+    development: Flags.boolean({
+      char: 'd',
+      description: 'Pull theme files from your remote development theme.',
+      env: 'SHOPIFY_FLAG_DEVELOPMENT',
+    }),
+    live: Flags.boolean({
+      char: 'l',
+      description: 'Pull theme files from your remote live theme.',
+      env: 'SHOPIFY_FLAG_LIVE',
+    }),
+    nodelete: Flags.boolean({
+      char: 'n',
+      description: 'Runs the pull command without deleting local files.',
+      env: 'SHOPIFY_FLAG_NODELETE',
+    }),
+    only: Flags.string({
+      char: 'o',
+      multiple: true,
+      description: 'Download only the specified files (Multiple flags allowed).',
+      env: 'SHOPIFY_FLAG_ONLY',
+    }),
+    ignore: Flags.string({
+      char: 'x',
+      multiple: true,
+      description: 'Skip downloading the specified files (Multiple flags allowed).',
+      env: 'SHOPIFY_FLAG_IGNORE',
+    }),
+    store: Flags.string({
+      char: 's',
+      description: 'Store URL',
+      env: 'SHOPIFY_FLAG_STORE',
+      parse: (input, _) => Promise.resolve(string.normalizeStoreName(input)),
+    }),
+  }
+
+  async run(): Promise<void> {
+    const {flags} = await this.parse(Pull)
+
+    let validPath = flags.path
+    if (!path.isAbsolute(validPath)) {
+      validPath = path.resolve(flags.path)
+    }
+
+    const flagsToPass = this.passThroughFlags(flags, {exclude: ['path', 'verbose']})
+
+    const command = ['theme', 'pull', validPath, ...flagsToPass]
+
+    const store = getTheme(flags)
+    const adminSession = await session.ensureAuthenticatedAdmin(store)
+    await execCLI2(command, {adminSession})
+  }
+}
