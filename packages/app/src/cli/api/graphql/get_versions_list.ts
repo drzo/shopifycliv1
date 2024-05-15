bb3aa39cf503e5356ce6ec1df8f77832378825e3
drzo <d@rzo.io>
2024-05-15 17:24:17 +0200

    Initialize shipit-deploy/stable_3_56

---
+++ packages/app/src/cli/api/graphql/get_versions_list.ts
@@ -0,0 +1,49 @@
+import {gql} from 'graphql-request'
+
+export const AppVersionsQuery = gql`
+  query AppVersionsQuery($apiKey: String!) {
+    app(apiKey: $apiKey) {
+      id
+      organizationId
+      title
+      appVersions {
+        nodes {
+          createdAt
+          distributionPercentage
+          createdBy {
+            displayName
+          }
+          message
+          status
+          versionTag
+        }
+        pageInfo {
+          totalResults
+        }
+      }
+    }
+  }
+`
+
+export interface AppVersionsQuerySchema {
+  app: {
+    id: string
+    organizationId: string
+    title: string
+    appVersions: {
+      nodes: {
+        createdAt: string
+        distributionPercentage: number
+        createdBy?: {
+          displayName?: string
+        }
+        message?: string
+        status: string
+        versionTag: string
+      }[]
+      pageInfo: {
+        totalResults: number
+      }
+    }
+  }
+}
