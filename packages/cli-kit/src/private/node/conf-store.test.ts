9a1b96fecf7b37e8064287d463db7e58ed97cf4d
drzo <d@rzo.io>
2024-05-15 16:53:45 +0200

    Initialize update-readme-oxygen-avail

---
+++ packages/cli-kit/src/private/node/conf-store.test.ts
@@ -0,0 +1,112 @@
+import {ConfSchema, cacheRetrieveOrRepopulate, getSession, removeSession, setSession} from './conf-store.js'
+import {LocalStorage} from '../../public/node/local-storage.js'
+import {describe, expect, it} from 'vitest'
+import {inTemporaryDirectory} from '@shopify/cli-kit/node/fs'
+
+describe('getSession', () => {
+  it('returns the content of the SessionStore key', async () => {
+    await inTemporaryDirectory(async (cwd) => {
+      // Given
+      const config = new LocalStorage<ConfSchema>({cwd})
+      config.set('sessionStore', 'my-session')
+
+      // When
+      const got = getSession(config)
+
+      // Then
+      expect(got).toEqual('my-session')
+    })
+  })
+})
+
+describe('setSession', () => {
+  it('saves the desired content in the SessionStore key', async () => {
+    await inTemporaryDirectory(async (cwd) => {
+      // Given
+      const config = new LocalStorage<ConfSchema>({cwd})
+      config.set('sessionStore', 'my-session')
+
+      // When
+      setSession('my-session', config)
+
+      // Then
+      expect(config.get('sessionStore')).toEqual('my-session')
+    })
+  })
+})
+
+describe('removeSession', () => {
+  it('removes the SessionStore key', async () => {
+    await inTemporaryDirectory(async (cwd) => {
+      // Given
+      const config = new LocalStorage<ConfSchema>({cwd})
+      config.set('sessionStore', 'my-session')
+
+      // When
+      removeSession(config)
+
+      // Then
+      expect(config.get('sessionStore')).toEqual(undefined)
+    })
+  })
+})
+
+describe('cacheRetrieveOrRepopulate', () => {
+  it('returns the cached contents when they exist', async () => {
+    await inTemporaryDirectory(async (cwd) => {
+      // Given
+      const config = new LocalStorage<ConfSchema>({cwd})
+      // populate the cache
+      await cacheRetrieveOrRepopulate('identity-introspection-url-IDENTITYURL', async () => 'URL1', 1000, config)
+
+      // When
+      const got = await cacheRetrieveOrRepopulate(
+        'identity-introspection-url-IDENTITYURL',
+        async () => 'URL2',
+        1000,
+        config,
+      )
+
+      // Then
+      // Uses the prior run to return the cached value
+      expect(got).toEqual('URL1')
+    })
+  })
+
+  it('derives the cached contents when the cache is not populated', async () => {
+    await inTemporaryDirectory(async (cwd) => {
+      // Given
+      const config = new LocalStorage<ConfSchema>({cwd})
+
+      // Then
+      const got = await cacheRetrieveOrRepopulate(
+        'identity-introspection-url-IDENTITYURL',
+        async () => 'URL1',
+        1000,
+        config,
+      )
+      expect(got).toEqual('URL1')
+    })
+  })
+
+  it('re-derives the cached contents when the cache is outdated', async () => {
+    await inTemporaryDirectory(async (cwd) => {
+      // Given
+      const config = new LocalStorage<ConfSchema>({cwd})
+
+      // When
+      // populate the cache
+      await cacheRetrieveOrRepopulate('identity-introspection-url-IDENTITYURL', async () => 'URL1', 1000, config)
+
+      // Then
+      const got = await cacheRetrieveOrRepopulate(
+        'identity-introspection-url-IDENTITYURL',
+        async () => 'URL2',
+        0,
+        config,
+      )
+      // Fetches a new value because the old one is outdated per the current request
+      expect(got).toEqual('URL2')
+    })
+  })
+})
