a922d6cb52688bbe0c470e6882e0febc88863500
drzo <d@rzo.io>
2024-05-15 16:49:03 +0200

    Initialize safi1012/fix_customer_accounts_deploy_translation

---
+++ packages/cli-kit/src/api/graphql/extension_specifications.ts
@@ -0,0 +1,43 @@
+import {gql} from 'graphql-request'
+
+export const ExtensionSpecificationsQuery = gql`
+  query fetchSpecifications($api_key: String!) {
+    extensionSpecifications(apiKey: $api_key) {
+      name
+      externalName
+      identifier
+      options {
+        managementExperience
+        registrationLimit
+      }
+      features {
+        argo {
+          surface
+        }
+      }
+    }
+  }
+`
+
+export interface ExtensionSpecificationsQueryVariables {
+  api_key: string
+}
+
+export interface RemoteSpecification {
+  name: string
+  externalName: string
+  identifier: string
+  options: {
+    managementExperience: 'cli' | 'custom' | 'dashboard'
+    registrationLimit: number
+  }
+  features?: {
+    argo?: {
+      surface: string
+    }
+  }
+}
+
+export interface ExtensionSpecificationsQuerySchema {
+  extensionSpecifications: RemoteSpecification[]
+}
