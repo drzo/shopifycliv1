ef5ef1b6b9339070390519a9d3c6b5cdef6fb66b
drzo <d@rzo.io>
2024-05-15 16:50:17 +0200

    Initialize ap.iqv-prototype

---
+++ packages/app/src/cli/models/extensions/extension-specifications/customer_accounts_ui_extension.ts
@@ -0,0 +1,37 @@
+import {createExtensionSpec} from '../extensions.js'
+import {BaseExtensionSchema} from '../schemas.js'
+import {schema, output} from '@shopify/cli-kit'
+
+const dependency = {name: '@shopify/customer-account-ui-extensions-react', version: '^0.0.20'}
+
+const CustomerAccountsSchema = BaseExtensionSchema.extend({
+  categories: schema.define.array(schema.define.string()).optional(),
+  localization: schema.define.any().optional(),
+})
+
+const spec = createExtensionSpec({
+  identifier: 'customer_accounts_ui_extension',
+  externalIdentifier: 'customer_accounts_ui',
+  externalName: 'Customer accounts UI',
+  surface: 'customer_accounts',
+  dependency,
+  partnersWebIdentifier: 'customer_accounts_ui_extension',
+  schema: CustomerAccountsSchema,
+  deployConfig: async (config, directory) => {
+    return {
+      extension_points: config.extensionPoints,
+      name: config.name,
+      categories: config.categories,
+    }
+  },
+  previewMessage: (host, uuid, _, storeFqdn) => {
+    const [storeName, ...storeDomainParts] = storeFqdn.split('.')
+    const accountsUrl = `${storeName}.account.${storeDomainParts.join('.')}`
+    const origin = encodeURIComponent(`${host}/extensions`)
+    const publicURL = `https://${accountsUrl}/extensions-development?origin=${origin}&extensionId=${uuid}`
+    const notice = `Please open ${host} and click on 'Visit Site' and then close the tab to allow connections.\n`
+    return output.content`${notice}Preview link: ${publicURL}`
+  },
+})
+
+export default spec
---
+++ packages/app/src/cli/services/dev/extension/server/utilities.ts
@@ -0,0 +1,32 @@
+import {UIExtension} from '../../../../models/app/extensions.js'
+import {getUIExtensionResourceURL} from '../../../../utilities/extensions/configuration.js'
+import {ExtensionDevOptions} from '../../extension.js'
+import {http} from '@shopify/cli-kit'
+
+export function getRedirectUrl(extension: UIExtension, options: ExtensionDevOptions): string {
+  const {url: resourceUrl} = getUIExtensionResourceURL(extension.configuration.type, options)
+
+  if (extension.surface === 'checkout' && resourceUrl) {
+    const rawUrl = new URL(`https://${options.storeFqdn}/`)
+    rawUrl.pathname = resourceUrl
+    rawUrl.searchParams.append('dev', `${options.url}/extensions`)
+
+    return rawUrl.toString()
+  } else {
+    const rawUrl = new URL(`https://${options.storeFqdn}/`)
+    rawUrl.pathname = 'admin/extensions-dev'
+    rawUrl.searchParams.append('url', getExtensionUrl(extension, options))
+
+    return rawUrl.toString()
+  }
+}
+
+export function getExtensionUrl(extension: UIExtension, options: ExtensionDevOptions): string {
+  const extensionUrl = new URL(options.url)
+  extensionUrl.pathname = `/extensions/${extension.devUUID}`
+  return extensionUrl.toString()
+}
+
+export function sendError(response: http.ServerResponse, error: Partial<http.H3Error>) {
+  http.sendError(response.event, http.createError(error))
+}
