2009239d1dba583182f497c07a46b0178d1bc63c
drzo <d@rzo.io>
2024-05-15 17:15:56 +0200

    Initialize add-app-handle-cli

---
+++ packages/app/src/cli/api/graphql/find_org.ts
@@ -0,0 +1,45 @@
+import {gql} from 'graphql-request'
+
+export const FindOrganizationQuery = gql`
+  query FindOrganization($id: ID!, $title: String) {
+    organizations(id: $id, first: 1) {
+      nodes {
+        id
+        businessName
+        website
+        apps(first: 25, title: $title) {
+          pageInfo {
+            hasNextPage
+          }
+          nodes {
+            id
+            title
+            handle
+            apiKey
+          }
+        }
+      }
+    }
+  }
+`
+
+export interface FindOrganizationQuerySchema {
+  organizations: {
+    nodes: {
+      id: string
+      businessName: string
+      website: string
+      apps: {
+        pageInfo: {
+          hasNextPage: boolean
+        }
+        nodes: {
+          id: string
+          title: string
+          handle: string
+          apiKey: string
+        }[]
+      }
+    }[]
+  }
+}
