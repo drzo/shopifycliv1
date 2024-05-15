5e899a3fa35f8d90f1385c5b38e92f9229195ecf
drzo <d@rzo.io>
2024-05-15 17:35:41 +0200

    Initialize experimental

---
+++ packages/theme/src/cli/commands/theme/share.ts
@@ -0,0 +1,40 @@
+import {themeFlags} from '../../flags.js'
+import {ensureThemeStore} from '../../utilities/theme-store.js'
+import ThemeCommand from '../../utilities/theme-command.js'
+import {execCLI2} from '@shopify/cli-kit/node/ruby'
+import {Flags} from '@oclif/core'
+import {globalFlags} from '@shopify/cli-kit/node/cli'
+import {ensureAuthenticatedThemes} from '@shopify/cli-kit/node/session'
+
+export default class Share extends ThemeCommand {
+  static summary = 'Creates a shareable, unpublished, and new theme on your theme library with a randomized name.'
+
+  static descriptionWithMarkdown = `Uploads your theme as a new, unpublished theme in your theme library. The theme is given a randomized name.
+
+  This command returns a [preview link](https://help.shopify.com/manual/online-store/themes/adding-themes#share-a-theme-preview-with-others) that you can share with others.`
+
+  static description = this.descriptionWithoutMarkdown()
+
+  static flags = {
+    ...globalFlags,
+    ...themeFlags,
+    force: Flags.boolean({
+      hidden: true,
+      char: 'f',
+      description: 'Proceed without confirmation, if current directory does not seem to be theme directory.',
+      env: 'SHOPIFY_FLAG_FORCE',
+    }),
+  }
+
+  static cli2Flags = ['force']
+
+  async run(): Promise<void> {
+    const {flags} = await this.parse(Share)
+    const flagsToPass = this.passThroughFlags(flags, {allowedFlags: Share.cli2Flags})
+
+    const store = ensureThemeStore(flags)
+    const adminSession = await ensureAuthenticatedThemes(store, flags.password)
+
+    await execCLI2(['theme', 'share', flags.path, ...flagsToPass], {store, adminToken: adminSession.token})
+  }
+}
