784deb1eab45940c8ff034a66d9621015e82ea2b
drzo <d@rzo.io>
2024-05-15 16:47:44 +0200

    Initialize web_pixel_rename_configuration_to_settings

---
+++ packages/cli-kit/src/constants.ts
@@ -0,0 +1,69 @@
+import {join as pathJoin} from './path.js'
+import {findPackageVersionUp} from './version.js'
+import envPaths from 'env-paths'
+
+const identifier = 'shopify-cli'
+
+const cacheFolder = () => {
+  if (process.env.XDG_CACHE_HOME) return process.env.XDG_CACHE_HOME
+  return envPaths(identifier).cache
+}
+
+const constants = {
+  environmentVariables: {
+    unitTest: 'SHOPIFY_UNIT_TEST',
+    env: 'SHOPIFY_ENV',
+    serviceEnv: 'SHOPIFY_SERVICE_ENV',
+    runAsUser: 'SHOPIFY_RUN_AS_USER',
+    spinInstance: 'SPIN_INSTANCE',
+    spinWorkspace: 'SPIN_WORKSPACE',
+    spinNamespace: 'SPIN_NAMESPACE',
+    spinHost: 'SPIN_HOST',
+    partnersToken: 'SHOPIFY_CLI_PARTNERS_TOKEN',
+    verbose: 'SHOPIFY_FLAG_VERBOSE',
+    noAnalytics: 'SHOPIFY_CLI_NO_ANALYTICS',
+    alwaysLogAnalytics: 'SHOPIFY_CLI_ALWAYS_LOG_ANALYTICS',
+    firstPartyDev: 'SHOPIFY_CLI_1P_DEV',
+    debugGoBinary: 'SHOPIFY_DEBUG_GO_BINARY',
+    deviceAuth: 'SHOPIFY_CLI_DEVICE_AUTH',
+    // Variables to detect if the CLI is running in a cloud environment
+    spin: 'SPIN',
+    codespaces: 'CODESPACES',
+    gitpod: 'GITPOD_WORKSPACE_ID',
+  },
+  paths: {
+    executables: {
+      dev: '/opt/dev/bin/dev',
+    },
+    directories: {
+      cache: {
+        path: () => {
+          return cacheFolder()
+        },
+        vendor: {
+          path: () => {
+            return pathJoin(cacheFolder(), 'vendor')
+          },
+          binaries: () => {
+            return pathJoin(cacheFolder(), 'vendor', 'binaries')
+          },
+        },
+      },
+    },
+  },
+  versions: {
+    cliKit: async () => {
+      return findPackageVersionUp({fromModuleURL: import.meta.url})
+    },
+  },
+  keychain: {
+    service: 'shopify-cli',
+  },
+  session: {
+    expirationTimeMarginInMinutes: 4,
+  },
+}
+
+export const bugsnagApiKey = '9e1e6889176fd0c795d5c659225e0fae'
+
+export default constants
---
+++ packages/ui-extensions-go-cli/CHANGELOG.md
@@ -0,0 +1,30 @@
+# @shopify/ui-extensions-go-cli
+
+## 3.11.0
+
+### Minor Changes
+
+- 81283e95: Move the extension creation business logic from the Go binary to the @shopify/app package
+
+## 3.10.1
+
+## 3.10.0
+
+## 3.9.2
+
+## 3.9.1
+
+## 3.9.0
+
+### Minor Changes
+
+- b97f2228: Fixed missing detailed error console output on extension build failure
+
+## 3.8.0
+
+### Minor Changes
+
+- 391f02a1: Don't install dependencies when creating a new extension
+- db4e6089: Releasing extensions binary in an automatic way
+
+## 3.7.1
