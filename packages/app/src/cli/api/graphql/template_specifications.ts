2f43268621138bb4d4f155c344e09cb96c47fb30
drzo <d@rzo.io>
2024-05-15 17:06:24 +0200

    Initialize remove-config-flag-from-link

---
+++ packages/app/src/cli/api/graphql/template_specifications.ts
@@ -0,0 +1,29 @@
+import {ExtensionTemplate} from '../../models/app/template.js'
+import {gql} from 'graphql-request'
+
+export const RemoteTemplateSpecificationsQuery = gql`
+  query RemoteTemplateSpecifications($version: String, $apiKey: String) {
+    templateSpecifications(version: $version, apiKey: $apiKey) {
+      identifier
+      name
+      defaultName
+      group
+      sortPriority
+      supportLinks
+      types {
+        url
+        type
+        extensionPoints
+        supportedFlavors {
+          name
+          value
+          path
+        }
+      }
+    }
+  }
+`
+
+export interface RemoteTemplateSpecificationsQuerySchema {
+  templateSpecifications: ExtensionTemplate[]
+}
---
+++ packages/app/src/cli/commands/app/config/link.ts
@@ -0,0 +1,32 @@
+import {appFlags} from '../../../flags.js'
+import link, {LinkOptions} from '../../../services/app/config/link.js'
+import Command from '../../../utilities/app-command.js'
+import {Flags} from '@oclif/core'
+import {globalFlags} from '@shopify/cli-kit/node/cli'
+
+const {config, ...appFlagsWithoutConfig} = appFlags
+
+export default class ConfigLink extends Command {
+  static description = 'Fetch your app configuration from the Partner Dashboard.'
+
+  static flags = {
+    ...globalFlags,
+    ...appFlagsWithoutConfig,
+    'client-id': Flags.string({
+      hidden: false,
+      description: 'The Client ID of your app.',
+      env: 'SHOPIFY_FLAG_CLIENT_ID',
+    }),
+  }
+
+  public async run(): Promise<void> {
+    const {flags} = await this.parse(ConfigLink)
+
+    const options: LinkOptions = {
+      commandConfig: this.config,
+      directory: flags.path,
+      apiKey: flags['client-id'],
+    }
+    await link(options)
+  }
+}
